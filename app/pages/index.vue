af76d4a57ad34bbb78dfefe2c61ff42b1ff45c73
Couavoux Baptiste <couavoux_baptiste@MBP-de-Couavoux.lan>
2025-11-17 22:57:36 +0100

    config eslint

---
+++ app/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <UContainer>
+    andoainzd
+  </UContainer>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,15 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default withNuxt(
-  // Your custom configs here
+  {
+    plugins: {
+      stylistic,
+    },
+
+    rules: {
+      'vue/no-multiple-template-root': 'off',
+    },
+  },
 )
