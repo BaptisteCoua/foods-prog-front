d196a5c603f22bc2ac7b2e7eacfd957395e68837
baptisteCoua <couavoux.b@gmail.com>
2026-07-01 00:02:25 +0200

    corrrection de l'animation des auth et register bug

--- technical/Auth/app/pages/register.vue
+++ technical/Auth/app/pages/register.vue
@@ -3,6 +3,6 @@
 </template>
 
 <script setup lang="ts">
-definePageMeta({ layout: 'auth' })
+definePageMeta({ layout: 'auth', pageTransition: { name: 'auth-page', mode: 'out-in' } })
 useHead({ title: 'Créer un compte' })
 </script>
